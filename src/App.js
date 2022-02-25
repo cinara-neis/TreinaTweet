b1e498ca3557d01a3ef15cc5693dc8fd3e76a787
Cinara Neis <80927757+cinara-neis@users.noreply.github.com>
2022-02-25 12:29:01 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Index from './ui/pages/index';
+import styles from './App.module.css';
+
+
+function App() {
+  return (
+    <div className={styles['app-container']}>
+      <Index/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/hooks/pages/useindex.page.js
@@ -0,0 +1,49 @@
+
+import {useApi} from '../useApi';
+import { mutate } from 'swr';
+import {useState, useMemo} from 'react';
+import { ApiService } from '../../services/ApiService';
+
+export  function useIndex() {
+    const maxTextLength = 125,
+    user = {
+        name: 'Cinara Neis',
+        username: 'cinara-neis',
+        picture: 'https://github.com/cinara-neis.png',
+        },
+        [text, setText] = useState(''),
+        tweetList = useApi('tweets').data,
+        sortedTweetList = useMemo(() => {
+          return (tweetList || []).sort((a,b) =>
+           a.data.date < b.data.date ? 1 : -1
+           );
+        }, [tweetList]);
+
+    function onTextChange(event){
+        const text = event.target.value;
+        if(text.length <= maxTextLength){
+            setText(text);
+        }
+    }
+
+    async function sendTweet(){
+       await ApiService.post('tweets', {
+            data: {
+                user,
+                text,
+                date: new Date().toISOString()
+            }
+        })
+        setText('');
+        mutate('tweets');
+    }
+
+    return {
+        user,
+        text,
+        onTextChange,
+        maxTextLength,
+        sendTweet,
+        sortedTweetList
+    };
+}
